48cf27cd2397f4a75d6210277e684a65b4b23a99
mo.duan <mo.duan@tendcloud.com>
2019-03-19 10:42:49 +0800

    add

--- template/src/main.js
+++ template/src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router/router'
 
 Vue.config.productionTip = false
+
+const a = 1;
+a = 2;
+
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
 Vue.use(iView);
